54533544c66a11497aefcb808853071afc499583
mdk <2049202552@qq.com>
2024-05-20 23:04:34 +0800

    style：去除element-icon的全局注册组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,11 @@
 // 注册全局组件
 import SvgIcon from './icons/SvgIcons.vue'
 import type { App, Component } from 'vue'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const components: { [name: string]: Component } = { SvgIcon }
 export default {
   install(app: App) {
     Object.keys(components).forEach((key: string) => {
       app.component(key, components[key])
     })
-
-    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-      app.component(key, component)
-    }
   }
 }
