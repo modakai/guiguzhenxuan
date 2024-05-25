04d889dfd4b10bb44577dd7f5720863c5f10fdc9
mdk <2049202552@qq.com>
2024-05-25 20:58:38 +0800

    feat：添加token路由守卫校验，未登入得无法进入后台页面

--- src/permission.ts
+++ src/permission.ts
@@ -2,13 +2,30 @@
 import router from './router'
 // 引入进度条
 import nprogress from '@/api/nprogress/nprogress'
+
+const ADMIN_TOKEN: string = 'ADMIN_TOKEN'
 // 全局路由前置路由守卫
+// to 是去哪个路由
+// from 是从哪个路由来
+// next 放行路由
 router.beforeEach((to, from, next) => {
-  // to 是去哪个路由
-  // from 是从哪个路由来
-  // next 放行路由
+  const token = localStorage.getItem(ADMIN_TOKEN)
   nprogress.start()
-  next()
+  if (token) {
+    // 登入成功说明local里面存在token
+    if (to.path === '/login') {
+      next({ path: '/' })
+    } else {
+      next()
+    }
+  } else {
+    console.log('to', to.path)
+    if (to.path === '/login') {
+      next()
+    } else {
+      next({ path: '/login', query: { redirect: to.path } })
+    }
+  }
 })
 
 // 全局路由后置守卫
